4d4dea965cb83f992c822a778c0bdc36d111461f
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-06-28 00:26:55 -0700

    using POSTGRES

--- django_ecommerce/django_ecommerce/settings.py
+++ django_ecommerce/django_ecommerce/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'djangousr',
+        'PASSWORD': 'meh',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
