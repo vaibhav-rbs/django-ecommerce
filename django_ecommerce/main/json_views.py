3561e1532d6b3ded8471cb7ca3a3602106c653f4
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-06 00:33:12 -0700

    class based REST API's

--- django_ecommerce/main/json_views.py
+++ django_ecommerce/main/json_views.py
@@ -1,39 +1,27 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import mixins, generics, request
 from main.serializer import StatusReportSerializer
 from main.models import StatusReport
 
+class StatusCollection(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = StatusReport.objects.all()
+    serializer_class = StatusReportSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def status_member(request, id):
-    """Get, update or delete a status_report instance"""
-    try:
-        status_report = StatusReport.objects.get(id=id)
-    except StatusReport.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == 'GET':
-        serializer = StatusReportSerializer(status_report)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = StatusReportSerializer(status_report, data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        status_report.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self, requst):
+        return self.list(request)
 
-@api_view(['GET','POST'])
-def status_collection(request):
-    if request.method == "GET":
-        status_report = StatusReport.objects.all()
-        serializer = StatusReportSerializer(status_report, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = StatusReportSerializer(data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, reqeust):
+        return self.create(reqeust)
+
+
+class StatusMember(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = StatusReport.objects.all()
+    serializer_class = StatusReportSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.put(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):    
+        return self.destroy(request, *args, **kwargs)
--- django_ecommerce/main/urls.py
+++ django_ecommerce/main/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import json_views
 urlpatterns = [
-    url(r'^status_reports/$',json_views.status_collection),
-    url(r'^status_reports/(?P<id>[0-9]+)$', json_views.status_member)
+    url(r'^status_reports/$',json_views.StatusCollection.as_view()),
+    url(r'^status_reports/(?P<pk>[0-9]+)$', json_views.StatusMember.as_view())
 ]
