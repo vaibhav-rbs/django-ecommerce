f8c442713f2eb62d2ff527a0a7a0645ca4cccba6
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-07-06 09:01:02 -0700

    REST API's Authorization.

--- django_ecommerce/main/json_views.py
+++ django_ecommerce/main/json_views.py
@@ -1,9 +1,11 @@
-from rest_framework import mixins, generics, request
+from rest_framework import mixins, generics, request, permissions
 from main.serializer import StatusReportSerializer
 from main.models import StatusReport
+from main.permissions import IsOwnerOrReadOnly
 
 class StatusCollection(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = StatusReport.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = StatusReportSerializer
 
     def get(self, requst):
@@ -15,6 +17,7 @@ class StatusCollection(mixins.ListModelMixin, mixins.CreateModelMixin, generics.
 
 class StatusMember(mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = StatusReport.objects.all()
+    permission_classes = (permissions.IsAuthenticated,IsOwnerOrReadOnly)
     serializer_class = StatusReportSerializer
 
     def get(self, request, *args, **kwargs):
